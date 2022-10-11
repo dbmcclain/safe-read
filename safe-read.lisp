d3033349d2fa9fa40995bedddcb13f29bda975ab
David McClain <dbmcclain@users.noreply.github.com>
2022-10-11 09:28:32 -0700

    3rd Proposed Fix...
    Have SAFE-READ return a third value on :INCOMPLETE-INPUT, being the buffered input needed for a repeat call to SAFE-READ. 
    
    Have SAFE-READ accept a third arg, BUFFER, defaulting to NIL, and should be the returned BUFFER third arg from the previous call to SAFE-READ, when you intend to re-read additional input from the stream to fix the :INCOMPLETE-INPUT condition.
    
    Normal use of SAFE-READ simply calls with none, one, or two args. Only rectifying repeat-reads need to supply the third arg.
    
    In this way, we make the caller responsible for the scope and lifetime of the stream. We do not cache anything locally, and hence no unsafe practices no matter what stream they throw at us.
    
    (I would still lobby for a second STRING, in addition to STREAM, entry point, to forego the need for line-oriented input when you already know the future.)

--- safe-read.lisp
+++ safe-read.lisp
@@ -7,7 +7,8 @@
 (in-package #:safe-read)
 
 ;; Exported conditions and parameters
-(define-condition incomplete-input () ())
+(define-condition incomplete-input ()
+  ((buffer  :accessor buffer-of  :initarg :buffer  :initform nil)))
 
 (define-condition malformed-input (error) ())
 
@@ -27,18 +28,6 @@
   (let ((whitespace '(#\Space #\Newline #\Backspace #\Tab
                       #\Linefeed #\Page #\Return #\Rubout)))
     (string-left-trim whitespace string)))
-
-;; Buffers for streams
-(defvar *stream-buffers* (make-weak-hash-table :weakness :key))
-
-(defun buffer-of (stream)
-  (check-type stream stream)
-  (or (gethash stream *stream-buffers*) ""))
-
-(defun (setf buffer-of) (new-value stream)
-  (if (or (null new-value) (string= new-value ""))
-      (remhash stream *stream-buffers*)
-      (setf (gethash stream *stream-buffers*) new-value)))
 
 ;; Utility macro - temporary packages
 (defmacro with-temp-package (&body body)
@@ -78,22 +67,27 @@
       (set-macro-character #\# #'eat-colon))))
 
 ;; Main exported function
-(defun safe-read (&optional (stream *standard-input*) use-list)
-  (let ((buffer (buffer-of stream)))
-    (handler-case
-        (if (string= "" buffer)
-            (safe-read-no-buffer stream use-list)
-            (safe-read-buffer stream use-list))
-      (incomplete-input ()
-        (values nil :incomplete-input))
-      (end-of-file (e)
-        (error e))
-      (error (error)
-        (setf (buffer-of stream) "")
-        (error error)))))
+(defun safe-read (&optional (stream *standard-input*) use-list buffer)
+  ;; NOTE: third arg, BUFFER, defaults to NIL. But on rereads against
+  ;; the same stream, it should be furnished as being the third return
+  ;; value from the prior call to SAFE-READ.
+  (handler-case
+      (if (or (null buffer)
+              (string= "" buffer))
+          (safe-read-no-buffer stream use-list)
+        (safe-read-buffer stream use-list buffer))
+    (incomplete-input (c)
+      (values nil :incomplete-input (buffer-of c)))
+    (end-of-file (e)
+      (error e))
+    (error (error)
+      (error error))))
 
 ;; Handler-case and macro-wrapper for safe reading
 (defmacro safe-read-handler-case (&body body)
+  ;; NOTE: I generally frown on intentional lexical capture inside of
+  ;; macros. But since you already adopted the style with binding
+  ;; "line", I will join you with binding "buffer". (DM/RAL 11/22)
   (let ((gensym (gensym)))
     `(with-temp-package ,@(if (eq (first body) :use-list)
                               (prog1 (list :use-list (second body))
@@ -101,28 +95,27 @@
        (handler-case
            (flet ((clear-buffer (e)
                     (declare (ignore e))
-                    (setf (buffer-of stream) "")))
-             (handler-bind ((malformed-input #'clear-buffer))
-               (let* ((*readtable* %safe-readtable%)
-                      (,gensym (progn ,@body)))
-                 (setf (buffer-of stream) "")
-                 (values ,gensym nil))))
+                    (setf buffer "")))
+               (handler-bind ((malformed-input #'clear-buffer))
+                 (let* ((*readtable* %safe-readtable%)
+                        (,gensym (progn ,@body)))
+                   (values ,gensym nil ""))))
          (end-of-file ()
            (unless (string= line "")
-             (setf (buffer-of stream)
-                   (uiop:strcat (buffer-of stream) line (string #\Newline))))
-           (signal (make-condition 'incomplete-input)))))))
+             (setf buffer
+                   (uiop:strcat buffer line (string #\Newline))))
+           (signal (make-condition 'incomplete-input :buffer buffer)))))))
 
 ;; Safe read - no buffer
-(defun safe-read-no-buffer (stream &optional use-list)
-  (let ((line (trim-leading-whitespace (read-limited-line stream))))
+(defun safe-read-no-buffer (stream use-list)
+  (let ((buffer "")
+        (line (trim-leading-whitespace (read-limited-line stream))))
     (safe-read-handler-case :use-list use-list
       (read-from-string line))))
 
 ;; Safe read - buffer
-(defun safe-read-buffer (stream &optional use-list)
-  (let* ((buffer (buffer-of stream))
-         (line (read-limited-line stream (length buffer))))
+(defun safe-read-buffer (stream use-list buffer)
+  (let ((line (read-limited-line stream (length buffer))))
     (safe-read-handler-case :use-list use-list
       (read-from-string (uiop:strcat buffer line)))))
 
